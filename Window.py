1d12d686a8c6982e5eb5fcdfbea20841bfaa736b
R8v3NN <dawn.priv@gmail.com>
2023-11-20 17:12:43 +0100

    Init

--- Window.py
+++ Window.py
@@ -3,16 +3,44 @@ import customtkinter
 customtkinter.set_appearance_mode("dark")
 
 window = customtkinter.CTk()
-window.geometry("1920x1280")
+#window.geometry("1920x1280")
+window.geometry("400x460")
+window.title("Fuzzy Logic")
 
 frame = customtkinter.CTkFrame(master=window)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
-def slider_time(value):
-    print(value)
 
-slider_time = customtkinter.CTkSlider(master=frame, command=slider_time, from_ = 0, to = 24, number_of_steps=24)
+def season_slider(value):
+    print(value)
+    lable_season.configure(text=f"Pora roku: {round(slider_season.get())}")
+def time_slider(value):
+    print(value)
+    lable_time.configure(text=f"Godzina: {round(slider_time.get())}")
+def room_temp_slider(value):
+    print(value)
+    lable_room_temp.configure(text=f"Temperatura Pokoju: {round(slider_room_temp.get())}")
+
+#GIT
+slider_season = customtkinter.CTkSlider(master=frame, command=season_slider, from_=1, to=4, number_of_steps=3)
+slider_season.pack(pady=12, padx=10)
+lable_season = customtkinter.CTkLabel(master=frame, text=f"Pora roku: {round(slider_season.get())}")
+lable_season.pack(pady=12, padx=10)
+
+slider_time = customtkinter.CTkSlider(master=frame, command=time_slider, from_=0, to=23, number_of_steps=23)
 slider_time.pack(pady=12, padx=10)
-slider_time.set(0.5)
+lable_time = customtkinter.CTkLabel(master=frame, text=f"Godzina: {round(slider_time.get())}")
+lable_time.pack(pady=12, padx=10)
+
+
+slider_room_temp = customtkinter.CTkSlider(master=frame, command=room_temp_slider, from_=0, to =30, number_of_steps=30)
+slider_room_temp.pack(pady=12, padx=10)
+lable_room_temp = customtkinter.CTkLabel(master=frame, text=f"Temperatura Pokoju: {round(slider_room_temp.get())}")
+lable_room_temp.pack(pady=12, padx=10)
+
+
+
+
+
 
 
 
