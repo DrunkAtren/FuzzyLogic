cf3a988438f31904ee5adaab13e997b90c093081
Atren <jakubbana@wp.pl>
2023-12-14 01:25:33 +0100

    FIN for day

--- main.py
+++ main.py
@@ -42,32 +42,28 @@ for i in range(0,47):
         goraco = 0
     TEMP[4, i] = goraco
 
-UST = np.zeros((6, 48))
-for i in range(0,47):
-    x=i-5
+UST = np.zeros((6, 100))
+for i in range(0,100):
+    x=i
     UST[0, i] = x
 
 
-    if x >= -5 and x <= 0:
-        UstZimna = 1
-    elif x >= 0 and x <= 10:
-        UstZimna = (10 - x) / (10 - 0)
+    if x >= 0 and x <= 50:
+        UstZimna = (50 - x) / (50 - 0)
     else:
         UstZimna = 0
     UST[1, i] = UstZimna
 
-    if x >= 0 and x <= 15:
-       UstSrednia = (x - 0) / (15 - 0)
-    elif x >= 15 and x <= 30:
-       UstSrednia = (30 - x) / (30 - 15)
+    if x >= 20 and x <= 60:
+       UstSrednia = (x - 20) / (60 - 20)
+    elif x >= 60 and x <= 80:
+       UstSrednia = (80 - x) / (80 - 60)
     else:
        UstSrednia = 0
     UST[2,i] = UstSrednia
 
-    if x >= 20 and x <= 30:
-        UstWysoka = (x - 20) / (30 - 20)
-    elif x >= 30 and x <=35:
-        UstWysoka = 1
+    if x >= 60 and x <= 100:
+        UstWysoka = (x - 60) / (100 - 60)
     else:
         UstWysoka = 0
     UST[3, i] = UstWysoka
@@ -89,7 +85,19 @@ plt.axis([-5,35,0,1.1])
 plt.plot(TEMP[0, :], TEMP[4, :], 'red')
 plt.axis([-5,35,0,1.1])
 
+plt.figure(2)
 
+plt.title('Ust')
+
+
+plt.plot(UST[0, :], UST[1, :], 'b')
+plt.axis([0,100,0,1.1])
+
+plt.plot(UST[0, :], UST[2, :], 'g')
+plt.axis([0,100,0,1.1])
+
+plt.plot(UST[0, :], UST[3, :], 'y')
+plt.axis([0,100,0,1.1])
 
 TempPokoju = 10+ 5
 
@@ -97,34 +105,15 @@ TempUstawiana = 20+5
 
 Zmiana = TempUstawiana-TempPokoju
 
-print(TEMP[1,TempPokoju], TEMP[2,TempPokoju], TEMP[3,TempPokoju], TEMP[4,TempPokoju])
+print(TEMP[1,Zmiana], TEMP[2,Zmiana], TEMP[3,Zmiana], TEMP[4,Zmiana])
 
-#TEMP1= Zimny / TEMP2= Letnio / TEMP3 = Cieplo / TEMP4 = Goraco
+#TEMP1= Zimno / TEMP2= Letnio / TEMP3 = Cieplo / TEMP4 = Goraco
 
 
-#r1 IF TempPokoju = Zimny AND Zmiana = Zimny THEN Zero
-#r2 IF TempPokoju = Zimny AND Zmiana = UstMaly THEN Maly
-#r3 IF TempPokoju = Zimny AND Zmiana = UstSrednia THEN Sredni
-#r4 IF TempPokoju = Zimny AND Zmiana = UstDuzy THEN Duzy
-#r5 IF TempPokoju = Zimny AND Zmiana = UstMax THEN Max
+#r1 IF Zmiana = Letnio  THEN Maly
+#r2 IF Zmiana = Cieplo  THEN Sredni
+#r3 IF Zmiana = Goraco  THEN Duzy
 
-#r6 IF TempPokoju = Letnio AND Zmiana = UstZimna THEN Zero
-#r7 IF TempPokoju = Letnio AND Zmiana = UstMaly THEN Zero
-#r8 IF TempPokoju = Letnio AND Zmiana = UstSrednia THEN Zero
-#r9 IF TempPokoju = Letnio AND Zmiana = UstDuzy THEN Duzy
-#r10 IF TempPokoju = Letnio AND Zmiana = UstMax THEN Max
-
-#r11 IF TempPokoju = Cieplo AND Zmiana = UstZimna THEN Zero
-#r12 IF TempPokoju = Cieplo AND Zmiana = UstMaly THEN Zero
-#r13 IF TempPokoju = Cieplo AND Zmiana = UstSrednia THEN Zero
-#r14 IF TempPokoju = Cieplo AND Zmiana = UstDuzy THEN Zero
-#r15 IF TempPokoju = Cieplo AND Zmiana = UstMax THEN Max
-
-#r16 IF TempPokoju = Goraco AND Zmiana = UstZimna THEN Zero
-#r17 IF TempPokoju = Goraco AND Zmiana = UstMaly THEN Zero
-#r18 IF TempPokoju = Goraco AND Zmiana = UstSrednia THEN Zero
-#r19 IF TempPokoju = Goraco AND Zmiana = UstDuzy THEN Zero
-#r20 IF TempPokoju = Goraco AND Zmiana = UstMax THEN Max
 
 
 T1 = TEMP[1,Zmiana]
@@ -132,61 +121,22 @@ T2 = TEMP[2,Zmiana]
 T3 = TEMP[3,Zmiana]
 T4 = TEMP[4,Zmiana]
 
-
-U2 = WYJ[2,Zmiana]
-U3 = WYJ[3,Zmiana]
-
-R1=min(T1,U2)
-R2=min(T1,U3)
-R3=min(T2,U2)
-R4=min(T2,U3)
-R5=min(T3,U3)
-
-R6=min(T1,U2)
-R7=min(T1,U3)
-R8=min(T2,U2)
-R9=min(T2,U3)
-R10=min(T3,U3)
-
-R11=min(T1,U2)
-R12=min(T1,U3)
-R13=min(T2,U2)
-R14=min(T2,U3)
-R15=min(T3,U3)
-
-R16=min(T1,U2)
-R17=min(T1,U3)
-R18=min(T2,U2)
-R19=min(T2,U3)
-R20=min(T3,U3)
-
-# US1 = WYJ[1,TempUstawiana]
-# US2 = WYJ[2,TempUstawiana]
-# US3 = WYJ[3,TempUstawiana]
-# US4 = WYJ[4,TempUstawiana]
-# US5 = WYJ[5,TempUstawiana]
+U1 = UST[1,Zmiana]
+U2 = UST[2,Zmiana]
+U3 = UST[3,Zmiana]
 
 
-Zero =[1,0,0,0,0]
-Maly =[0,1,0,0,0]
-Sredni =[0,0,1,0,0]
-Duzy =[0,0,0,1,0]
-Max =[0,0,0,0,1]
+
+R1=min(T2,U1)
+R2=min(T3,U2)
+R3=min(T4,U3)
+
+
+
 
 # print (US1,US2,US3,US4)
-print (R1,R2,R3,R4,R5)
-if R3!=0:
-    Maly =[min(0.5,R3),min(1,R3),min(0.5,R3),min(0,R3),min(0,R3),min(0,R3)]
+print (R1,R2,R3)
 
-if R1!=0:
-    Sredni = [min(0,R1),min(0.5,R1),min(1,R1),min(0.5,R1),min(0,R1),min(0,R1)]
-
-if  R2!=0:
-    Duzy = [min(0, R2), min(0, R2), min(0, R2), min(0.5, R2), min(1, R2)]
-elif R4!=0:
-    Duzy = [min(0, R4), min(0, R4), min(0, R4), min(0.5, R4), min(1, R4)]
-elif R5!=0:
-    Duzy = [min(0, R5), min(0, R5), min(0, R5), min(0.5, R5), min(1, R5)]
 
 
 
