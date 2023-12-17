8c7f67d76ce62e4c2c60f3f48c183786f99a4728
Atren <jakubbana@wp.pl>
2023-12-17 20:51:41 +0100

    Com

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-TEMP = np.zeros((6, 48))
+
+
+
+TEMP = np.zeros((5, 48))
 for i in range(0,47):
-    x=i-5
+    x=i
     TEMP[0, i] = x
 
     #temperatura zimno
@@ -42,31 +46,42 @@ for i in range(0,47):
         goraco = 0
     TEMP[4, i] = goraco
 
-UST = np.zeros((6, 100))
-for i in range(0,100):
+UST = np.zeros((6, 110))
+for i in range(0,110):
     x=i
     UST[0, i] = x
 
-
-    if x >= 0 and x <= 50:
-        UstZimna = (50 - x) / (50 - 0)
+    if x == 0:
+        UstWyl = 1
     else:
-        UstZimna = 0
-    UST[1, i] = UstZimna
+        UstWyl = 0
+    UST[1, i] = UstWyl
 
-    if x >= 20 and x <= 60:
-       UstSrednia = (x - 20) / (60 - 20)
-    elif x >= 60 and x <= 80:
-       UstSrednia = (80 - x) / (80 - 60)
+    if x == 25:
+        UstMaly = 1
     else:
-       UstSrednia = 0
-    UST[2,i] = UstSrednia
+        UstMaly = 0
+    UST[2, i] = UstMaly
 
-    if x >= 60 and x <= 100:
-        UstWysoka = (x - 60) / (100 - 60)
+    if x == 50:
+        UstSredni = 1
     else:
-        UstWysoka = 0
-    UST[3, i] = UstWysoka
+        UstSredni = 0
+    UST[3, i] = UstSredni
+
+    if x == 75:
+        UstDuzy = 1
+    else:
+        UstDuzy = 0
+    UST[4, i] = UstDuzy
+
+    if x == 100:
+        UstMax = 1
+    else:
+        UstMax = 0
+    UST[5, i] = UstMax
+
+
 plt.figure(1)
 
 plt.xlabel('Temperatura [C]')
@@ -93,52 +108,70 @@ plt.title('Ust')
 plt.plot(UST[0, :], UST[1, :], 'b')
 plt.axis([0,100,0,1.1])
 
-plt.plot(UST[0, :], UST[2, :], 'g')
+plt.plot(UST[0, :], UST[2, :], 'b')
 plt.axis([0,100,0,1.1])
 
-plt.plot(UST[0, :], UST[3, :], 'y')
+plt.plot(UST[0, :], UST[3, :], 'b')
 plt.axis([0,100,0,1.1])
 
-TempPokoju = 10+ 5
+plt.plot(UST[0, :], UST[4, :], 'b')
+plt.axis([0,100,0,1.1])
 
-TempUstawiana = 20+5
+plt.plot(UST[0, :], UST[5, :], 'b')
+plt.axis([0,100,0,1.1])
 
-Zmiana = TempUstawiana-TempPokoju
+TempZew = 15
+TempPiec = 0
+TempPokoju = TempZew + TempPiec
+#Pokoj
+TempUstawiana = 20
 
-print(TEMP[1,Zmiana], TEMP[2,Zmiana], TEMP[3,Zmiana], TEMP[4,Zmiana])
 
 #TEMP1= Zimno / TEMP2= Letnio / TEMP3 = Cieplo / TEMP4 = Goraco
 
+timeout = 5.0
 
-#r1 IF Zmiana = Letnio  THEN Maly
-#r2 IF Zmiana = Cieplo  THEN Sredni
-#r3 IF Zmiana = Goraco  THEN Duzy
+def xyz():
+    global Zmiana
+    global TempPiec
+    global TempPokoju
+    global TempUstawiana
 
-
-
-T1 = TEMP[1,Zmiana]
-T2 = TEMP[2,Zmiana]
-T3 = TEMP[3,Zmiana]
-T4 = TEMP[4,Zmiana]
-
-U1 = UST[1,Zmiana]
-U2 = UST[2,Zmiana]
-U3 = UST[3,Zmiana]
+    TempPokoju = TempPokoju - 1 + TempPiec
+    Zmiana = TempUstawiana - TempPokoju
 
 
 
-R1=min(T2,U1)
-R2=min(T3,U2)
-R3=min(T4,U3)
+    print("-----")
+    print(TempPiec)
 
+    print(TempPokoju)
+    print(Zmiana)
+    print(TEMP[1, Zmiana], TEMP[2, Zmiana], TEMP[3, Zmiana], TEMP[4, Zmiana])
 
+    R1 = TEMP[1, Zmiana]
+    R2 = TEMP[2, Zmiana]
+    R3 = TEMP[3, Zmiana]
+    R4 = TEMP[4, Zmiana]
 
+    if Zmiana<0:
+        TempPiec = 0
+    elif R1 > 0:
+        TempPiec = 2
+    elif R2 > 0:
+        TempPiec = 3
+    elif R3 > 0:
+        TempPiec = 4
+    elif R4 > 0:
+        TempPiec = 5
+    time.sleep(5)
 
-# print (US1,US2,US3,US4)
-print (R1,R2,R3)
-
-
-
-
+while True:
+    xyz()
 
 plt.show()
+#r1 IF Zmiana = Zimno THEN Maly
+#r2 IF Zmiana = Letnio  THEN Sredni
+#r3 IF Zmiana = Cieplo  THEN Duzy
+#r4 IF Zmiana = Goraco  THEN Max
+#r5 IF Zmiana<0 THEN Wylacz
